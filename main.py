59c793607c864379059c7f9bb52bcccf71c7fec6
MKinga08 <dr.m.kinga08@gmail.com>
2022-10-31 13:41:08 +0100

    @ validator implemented

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
-def get_users_input():
-    email = input("What is your email address?")
-    return email
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def at_validator():
+    while True:
+        address = input("What is your email address?")
+        if address.count("@") == 1:
+            return address
+        elif address.count("@") > 1:
+            print("You can only have one @ in your email address")
+        else:
+            print("You need to have one @ in your email address")
 
 
 def main():
-
-    get_users_input()
+    at_validator()
 
 
 if __name__ == '__main__':
