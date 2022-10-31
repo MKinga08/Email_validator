81186a3b13a580fd07f4e1bd8bd387e5a8061afb
MKinga08 <dr.m.kinga08@gmail.com>
2022-10-31 12:52:34 +0100

    creating main and get users input

---
+++ main.py
@@ -0,0 +1,29 @@
+def get_users_input():
+    email = input("What is your email address?")
+    return email
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+    get_users_input()
+
+
+if __name__ == '__main__':
+    main()
