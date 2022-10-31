d80d48130187586ab9ec1317b629da3474f6d3ff
MKinga08 <dr.m.kinga08@gmail.com>
2022-10-31 20:57:16 +0100

    special character validator implemented

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def get_users_input():
+    address = input("What is your email address?")
+    return address
+
+
 def at_validator():
     while True:
-        address = input("What is your email address?")
+        address = get_users_input()
         if address.count("@") == 1:
             return address
         elif address.count("@") > 1:
@@ -9,8 +14,35 @@ def at_validator():
             print("You need to have one @ in your email address")
 
 
+def special_char_validator(address):
+    if "@" in address:
+        username, domain = address.split("@")
+        if username == "":
+            print("You must enter a username")
+        if domain == "":
+            print("You must enter a domain")
+        c = 0
+        s = "[@_!#$%^&*()<>?/}{~:|]"
+        for i in range(len(username)):
+            if username[i] in s:
+                c += 1
+        if c > 0:
+            print("username should not contain any special characters")
+        k = 0
+        sp = "[@_!#$%^&*()<>?/}{~:|]"
+        for j in range(len(domain)):
+            if domain[j] in sp:
+                k += 1
+        if k > 0:
+            print("domain should not contain any special characters")
+    return True
+
+
 def main():
-    at_validator()
+    while True:
+        email = at_validator()
+        if special_char_validator(email):
+            break
 
 
 if __name__ == '__main__':
